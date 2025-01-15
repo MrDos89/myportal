80d9d1127766b603896156a7251d4a9b274ddec7
김도형 <mrdos@naver.com>
2025-01-15 14:29:18 +0900

    [도형] 게시판 1차 업로드

---
+++ src/main/java/himedia/myportal/controllers/BoardController.java
@@ -0,0 +1,54 @@
+package himedia.myportal.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import himedia.myportal.repositories.vo.BoardVo;
+import himedia.myportal.services.BoardService;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	@Autowired
+	private BoardService boardServiceImpl;
+	
+	@RequestMapping({"","/","list"})
+	public String list(Model model) {
+		List<BoardVo> list = boardServiceImpl.getList();
+		
+		model.addAttribute("list", list);
+		
+		return "board/list";
+	}
+	
+	@GetMapping("/write")
+	public String writeForm() {
+		return "board/write";
+	}
+	
+	@PostMapping("/write")
+	public String writeAction (@ModelAttribute BoardVo vo) {
+		System.out.println("FORM:" + vo);
+		boolean success = boardServiceImpl.write(vo);
+		System.out.println("Write Result:" + success);
+		
+		return "redirect:/guestbook";
+	}
+	
+	//게시물 조회
+	@GetMapping("/{no}")
+	public String view(@PathVariable("no") Integer no, Model model) {
+		BoardVo vo = boardServiceImpl.getContent(no);
+		model.addAttribute("vo", vo);
+		
+		return "board/view";
+	}
+}
